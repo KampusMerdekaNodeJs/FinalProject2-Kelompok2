fbce651292d1d57e9d6beaf3fa30e397e29d489f
Dzikri52464 <111979924+Dzikri52464@users.noreply.github.com>
2023-05-12 22:33:07 +0700

    Delete routers directory

--- routers/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const photo = require('./photoRouter');
-const user = require('./userRouter')
-
-router.use('/',user)
-router.use('/',photo)
-
-module.exports = router;
